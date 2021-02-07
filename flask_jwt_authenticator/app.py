abd137d49eda8950c015ead1dfd06cff88f90647
Kavuti <christian.cavuti@gmail.com>
2021-02-07 23:40:26 +0100

    Implementing a registration endpoint

--- flask_jwt_authenticator/app.py
+++ flask_jwt_authenticator/app.py
@@ -2,7 +2,7 @@ import os
 import hashlib
 import logging
 from db import db
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_jwt import JWT, jwt_required
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import safe_str_cmp
@@ -37,6 +37,50 @@ jwt = JWT(app, authenticate, identity)
 def get_identity():
     return f"{current_identity}"
 
+@app.route('/register', methods=['POST'])
+def register():
+    user_data = request.get_json()
+    if not user_data:
+        return jsonify({'status': 'fail', 
+                        'message': 'Data must be provided',
+                        }), 400
+
+    if not 'username' in user_data or not user_data['username']:
+        return jsonify({
+            'status': 'fail',
+            'message': 'The username must be specified'
+        }), 400
+    if not 'email' in user_data or not user_data['email']:
+        return jsonify({
+            'status': 'fail',
+            'message': 'The email must be specified'
+        }), 400
+    if not 'password' in user_data or not user_data['password']:
+        return jsonify({
+            'status': 'fail',
+            'message': 'A password must be specified'
+        }), 400
+
+    already_taken = User.query.filter((User.username==user_data['username']) | (User.email==user_data['email'])).first()
+    if already_taken:
+        return jsonify({
+            'status': 'fail',
+            'message': 'The username is already taken'
+        }), 422
+    
+    user_object = User(user_data['username'], user_data['email'], user_data['password'])
+    user_object.save()
+    return jsonify({
+        'status': 'success',
+        'user': user_object
+    }), 200
+
+@app.errorhandler(500)
+def on_error(error):
+    return jsonify({
+        'status': 'error',
+        'message': 'Internal error'
+    }), 500
 
 if __name__ == "__main__":
     app.run()
--- flask_jwt_authenticator/user.py
+++ flask_jwt_authenticator/user.py
@@ -4,4 +4,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
